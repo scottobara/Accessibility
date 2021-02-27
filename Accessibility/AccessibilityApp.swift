d65857f0c475876bf1ab1ae18c2b658e46a8edcb
Scott Obara <scott@rubbscreations.com>
2021-02-28 09:07:54 +1100

    Initial Commit

---
+++ Accessibility/AccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityApp.swift
+//  Accessibility
+//
+//  Created by Scott Obara on 28/2/21.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
