1ee055986b53c62f3a13b6861d77eb7f8e4aa72e
Scott Obara <scott@rubbscreations.com>
2021-02-28 10:27:28 +1100

    accessibilityElement and accessibility modifiers

--- Accessibility/ContentView.swift
+++ Accessibility/ContentView.swift
@@ -8,32 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pictures = [
-        "ales-krivec-15949",
-        "galina-n-189483",
-        "kevin-horstmann-141705",
-        "nicolas-tissot-335096"
-    ]
     
-    let labels = [
-        "Tulips",
-        "Frozen tree buds",
-        "Sunflowers",
-        "Fireworks",
-    ]
-
-    @State private var selectedPicture = Int.random(in: 0...3)
-
     var body: some View {
-        Image(pictures[selectedPicture])
-            .resizable()
-            .scaledToFit()
-            .accessibility(label: Text(labels[selectedPicture]))
-            .accessibility(addTraits: .isButton)
-            .accessibility(removeTraits: .isImage)
-            .onTapGesture {
-                self.selectedPicture = Int.random(in: 0...3)
+        HStack {
+            VStack {
+                Text("Your score is")
+                Text("1000")
+                    .font(.title)
             }
+            .accessibilityElement(children: .combine)
+            //.accessibility(label: Text("Your score is 1000"))
+            VStack {
+                Text("Your score is")
+                Text("1000")
+                    .font(.title)
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibility(label: Text("Your score is 1000"))
+        }
     }
 }
 
